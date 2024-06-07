2f8458fba429827dd3d00ec3b982ed2399b39e73
Keith Heyboer <heyboeke@mail.gvsu.edu>
2024-06-07 19:56:11 -0400

    Use UUIDs for records

--- server/models/Records.js
+++ server/models/Records.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose")
+const { v4: uuidv4 } = require("uuid")
+
 const RecordsSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        unique: true,
+        default: uuidv4, // generate a new UUID as default value
+    },
     user: {
         type: String,
         required: true,
