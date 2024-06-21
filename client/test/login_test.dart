85111a08fe83826be135ad148d27ce32cfd12c14
Keith Heyboer <heyboeke@mail.gvsu.edu>
2024-06-21 12:44:51 -0400

    Add more client tests

--- client/test/login_test.dart
+++ client/test/login_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 // import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:sleepapp/main.dart';
@@ -13,14 +14,39 @@ import 'package:sleepapp/main.dart';
 void main() {
   testWidgets('Login Page Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
+    //Looks for Text
     expect(find.text("Login"), findsOneWidget);
     expect(find.text("Don't have an account? Create one"), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    // await tester.tap(find.byIcon(Icons.add));
-    // await tester.pump();
+    //sign in button
+    var button = find.text("Sign In");
+    expect(button, findsOneWidget);
+    await tester.tap(button);
+    await tester.pump();
+    //checks for empty login
+    expect(find.text("Username must be at least 1 character"), findsOneWidget);
+
+    //Button only works the first time, last line of next two text grayed out for now
+    //tried adding a delay since the bottom only pops up for a moment, doenst work
+    await tester.pump(new Duration(milliseconds: 200000));
+
+    //sign in with no password
+    final username = find.ancestor(
+        of: find.text('Username'), matching: find.byType(TextField));
+    await tester.enterText(username, "TestAcc");
+    expect(find.text("TestAcc"), findsOneWidget);
+    await tester.tap(button);
+    await tester.pump();
+    //expect(find.text("Password must be at least 3 characters"), findsOneWidget);
+
+    //sign in with no username [Should test for bullets in password but dont know how]
+    final password = find.ancestor(
+        of: find.text('Username'), matching: find.byType(TextField));
+    await tester.enterText(password, "Password");
+    await tester.tap(button);
+    await tester.pump();
+    // expect(find.text("Username must be at least 1 character"), findsOneWidget);
   });
 }
