147f645ddb234fc01f501feb7953f0fde052a064
Keith Heyboer <heyboeke@mail.gvsu.edu>
2024-06-21 00:12:21 -0400

    Reorganize the server for testing

---
+++ server/tests/app.test.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const request = require("supertest");
+
+const app = require("../app");
+
+require("dotenv").config();
+
+beforeEach(async () => {
+  await mongoose.connect(process.env.DATABASE_URL);
+});
+
+afterEach(async () => {
+  await mongoose.connection.close();
+});
+
+describe("GET /getUser", () => {
+  it("should return user", async () => {
+    const response = await request(app).get("/getUser").send({
+      user: "server test",
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("POST /loginUser", () => {
+  it("should login as user", async () => {
+    const response = await request(app).post("/loginUser").send({
+      user: "server test",
+      pass: "server test"
+    });
+
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("POST /loginUser with invalid credentials", () => {
+  it("should fail to login", async () => {
+    const response = await request(app).post("/loginUser").send({
+      user: "server test",
+      pass: "something else"
+    });
+
+    expect(response.statusCode).toBe(401);
+  });
+});
